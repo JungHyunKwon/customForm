9ed477d588ff347288ef18737bc6df3c485bccf7
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-08-15 20:49:19 +0900

    published

---
+++ README.md
@@ -0,0 +1,44 @@
+# customForm v1.0.0
+폼요소를 쉽게 커스텀할 수 있는 플러그인입니다.
+
+## 지원요소
+- select
+- select[multiple]
+- input[type='checkbox']
+- input[type='radio']
+- input[type='file']
+- input[type='file'][multiple]
+
+## 클래스
+- active : 활성화되었을때 부여됩니다.
+- disabled : 폼요소에 disabled 속성이 있을때 지정됩니다.
+
+[data-custom]에 부여됩니다.
+
+## 네임스페이스
+- customForm
+
+### 활성화되는 순간
+- select : 포커스가 들어갔을때, 나갔을때
+- input[type='checkbox'] : 변경되었을때
+- input[type='radio'] : 변경되었을때
+- input[type='file'] : 파일이 선택되어 있을때
+
+### data-*
+- data-custom : [data-custom-text]와 폼요소의 부모이며 값은 폼요소 구분자로 넣어주세요.
+  - 예시 : select, select_multiple, checkbox, radio, file, file_multiple
+- data-custom-text : [data-custom]에 존재해야하며 상태에 따른 문자를 바꿀 요소입니다.
+- data-custom-text-overflow : [data-custom-text]에 지정가능하며 넓이값을 지정하면 말줄임이 생성됩니다.
+
+### 참고사항
+- 내부에 필요한만큼 요소를 감쌀 수 있습니다.
+- [data-custom]안에 [data-custom]은 올 수 없습니다.
+- [data-custom]안에 폼요소를 한개만 넣어주세요.
+- data-* 속성은 유지한채 요소변경 가능합니다.
+
+## 제이쿼리 개발버전
+1.12.4
+
+## 브라우저 지원
+- js : ie7이상 그 이외 브라우저 모두 지원합니다.
+- css : ie10이상 그 이외 브라우저 모두 지원합니다.
---
+++ js/jquery.customForm.js
@@ -0,0 +1,156 @@
+/**
+ * @name customForm
+ * @author JungHyunKwon
+ * @version 1.0.0
+ */
+try {
+	'use strict';
+
+	(function($) {
+		//제이쿼리가 함수일때
+		if(typeof $ === 'function') {
+			$(function() {
+				var $custom = $('[data-custom]'),
+					$customElement = $custom.find('select, input[type="checkbox"], input[type="radio"], input[type="file"]'),
+					$reset = $('[type="reset"]').filter('input, button');
+				
+				$customElement.on('change.customForm', function(event) {
+					var $this = $(this),
+						$custom = $this.parents('[data-custom]'),
+						$customText = $custom.find('[data-custom-text]'),
+						customText = $customText.attr('data-custom-text') || '',
+						tagName = this.tagName.toLowerCase(),
+						type = this.type.toLowerCase(),
+						separator = ', ',
+						text = '';
+					
+					//셀렉트일때
+					if(tagName === 'select') {
+						var $selectedOption = $this.find('option:selected');
+
+						$selectedOption.each(function(index, element) {
+							text += $(element).text() + separator;
+						});
+
+						text = text.substring(0, text.length - separator.length);
+
+						//값이 없을때
+						if(!text) {
+							text = customText;
+						}
+
+						$customText.text(text);
+					
+					//인풋일때
+					}else if(tagName === 'input') {
+						//체크박스일때
+						if(type === 'checkbox') {
+							//체크되어 있을때
+							if($this.is(':checked')) {
+								$custom.addClass('active');
+							}else{
+								$custom.removeClass('active');
+							}			
+						
+						//라디오일때
+						}else if(type === 'radio') {
+							var $parentsForm = $this.parents('form');
+
+							$customElement.filter('input[type="radio"][name="' + (this.name || '') + '"]').each(function(index, element) {
+								var $element = $(element),
+									$custom = $element.parents('[data-custom]'),
+									$form = $element.parents('form');
+								
+								//체크되어 있을때
+								if($element.is(':checked')) {
+									$custom.addClass('active');
+								
+								//선택된 라디오의 부모가 없으면서 부모에 폼이 없으거나 선택된 라디오의 부모가 선택된 라디오의 부모의 폼과 같을때
+								}else if((!$parentsForm.length && !$form.length) || $parentsForm.is($form)) {
+									$custom.removeClass('active');
+								}
+							});
+						
+						//파일일때
+						}else if(type === 'file') {
+							var files = this.files || this.value;
+							
+							//파일이 없을때
+							if(!files) {
+								files = [];
+							}
+
+							//문자일때
+							if(typeof files === 'string') {
+								files = [files];
+							}
+
+							$.each(files, function(index, file) {
+								text += (file.name || file) + separator;
+							});
+
+							text = text.substring(0, text.length - separator.length);
+
+							//값이 없을때
+							if(!text) {
+								text = customText;
+							}
+
+							$customText.text(text);
+							
+							//파일이 선택되어 있을때
+							if(files.length) {
+								$custom.addClass('active');	
+							}else{
+								$custom.removeClass('active');
+							}
+						}
+					}
+				}).on('focusin.customForm', function(event) {
+					var $this = $(this),
+						tagName = this.tagName.toLowerCase();
+					
+					//셀렉트일때
+					if(tagName === 'select') {
+						$this.parents('[data-custom]').addClass('active');
+					}
+				}).on('focusout.customForm', function(event) {
+					var $this = $(this),
+						tagName = this.tagName.toLowerCase();
+					
+					//셀렉트일때
+					if(tagName === 'select') {
+						$this.parents('[data-custom]').removeClass('active');
+					}
+				}).each(function(index, element) {
+					var $element = $(element);
+					
+					//비활성화일때
+					if($element.attr('disabled')) {
+						$element.parents('[data-custom]').addClass('disabled');
+					}
+
+					$element.triggerHandler('change.customForm');
+				});
+
+				//초기화
+				$reset.on('click.customForm', function(event) {
+					var $this = $(this),
+						$form = $this.parents('form');
+					
+					$form[0].reset();
+
+					$form.find('[data-custom]').find('select, input[type="checkbox"], input[type="radio"], input[type="file"]').each(function(index, element) {
+						$(element).triggerHandler('change.customForm');
+					});
+
+					event.preventDefault();
+				});
+			});
+		}else{
+			throw '제이쿼리가 없습니다.';
+		}
+	})(window.jQuery);
+}catch(error) {
+	console.error(error);
+}
