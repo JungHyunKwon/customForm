ec11e576126f65c7ba43ab6fd13a6806d9ca7bab
JungHyunKwon <kjh_3859@naver.com>
2019-07-14 04:48:24 +0900

    published

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# customForm v1.0.0
-폼 요소를 커스텀 할 수 있는 플러그인입니다.
+# label v1.0.0
+라벨을 꾸밀 수 있는 플러그인입니다.
 
 ## 지원 요소
 - select
@@ -10,42 +10,40 @@
 - input[type='file'][multiple]
 
 ## 클래스
-- active : 활성화되었을 때 부여됩니다.
-- focus : 포커스되었을 때 부여됩니다.
+- active : 활성화
+- focus : 포커스
 
-.custom_form에 부여됩니다.
+.label에 부여됩니다.
 
 ## 구조
-- .custom_form : .custom_text와 .custom_item 요소의 부모이며 클래스로 구분자를 넣어주세요.
-  - 예시 : .custom_select, .custom_checkbox, .custom_radio, .custom_file
-- .multiple : 지원 요소에 multiple 속성이 있을 때 .custom_form에 클래스를 넣어주세요.
-- .custom_text : 상태에 따른 문자를 바꿀 요소이며 선택된 값이 없을 때 요소안의 내용이 기본값이 됩니다.
-- .custom_item : 지원 요소에 클래스를 넣어주세요.
+- .label : .label_text와 .label_item 요소의 부모이며 클래스로 구분자를 넣어주세요.
+  - 예시 : .select, .multiple, .checkbox, .radio, .file
+- .label_text : 라벨
+- .label_item : 서식 요소의 클래스
 
 ## 메서드
 
 ```javascript
-    $(selector).customForm(method, value);
+    $(selector).label(method, value);
 ````
 
 이름 | 매개변수 | 반환 | 설명
 | :-- | :---- | :-- | :-- |
-refresh | | jQuery | 새로고침
-destroy | | jQuery | 소멸
-focus | | jQuery | 포커스
-reset | | jQuery | 초기화
-val | string | jQuery \|\| array [object {$element : jQuery, value : array [string]}] \|\| string | 값을 지정하거나 값을 얻을 수 있습니다.
-defaultText | string | jQuery \|\| array [object {$element : jQuery, value : array [string]}] \|\| string | 기본값을 지정하거나 기본값을 얻을 수 있습니다.
+refresh | | object | 새로고침
+destroy | | object | 소멸
+focus | | object | 포커스
+reset | | object | 초기화
+val | string | object \|\| array [object {$element : jQuery, value : array [string]}] \|\| string | 값을 지정하거나 값을 얻을 수 있습니다.
+default | string | jQuery \|\| array [object {$element : jQuery, value : array [string]}] \|\| string | 기본값을 지정하거나 기본값을 얻을 수 있습니다.
 
 ## 참고사항
-- .custom_form 안에 .custom_text와 .custom_item이 각 1개씩만 존재해야 합니다.
-- .custom_form 안에 .custom_form은 올 수 없습니다.
-- 내부에 필요한 만큼 요소를 감쌀 수 있으며 .custom_text에는 감쌀 수 없습니다.
-- 기본값은 .custom_text의 값 입니다.
+- .label 안에 .label_text와 .label_item이 각 1개씩만 존재해야 하며 중첩할 수 없습니다.
+- 내부에 필요한 만큼 요소를 감쌀 수 있습니다.
+- 기본값은 .label_text의 값 입니다.
 
 ## 제이쿼리 개발 버전
 1.12.4
 
 ## 브라우저 지원
-- js : ie7 이상 그 이외 브라우저 모두 지원합니다.
-- css : ie9 이상 그 이외 브라우저 모두 지원합니다.
+- js : 인터넷 익스플로러7 이상
+- css : 인터넷 익스플로러9 이상
---
+++ js/jquery.label.js
@@ -0,0 +1,236 @@
+/**
+ * @author JungHyunKwon
+ * @since 2017-12-06
+ * @version 1.0.0
+ */
+(function($) {
+	'use strict';
+
+	var _defaultLabelText;
+
+	/**
+	 * @name label
+	 * @param {string} method
+	 * @param {string} value
+	 * @return {object || string || array [object {
+		   $element : jQuery,
+		   value : array [string] || string
+	   }]}
+	 */
+	$.fn.label = function(method, value) {
+		var valueIsString = typeof value === 'string',
+			hasValue = arguments.hasOwnProperty(1),
+			result = [];
+
+		this.each(function(index, element) {
+			var $element = $(element),
+				$labelText = $element.find('.label_text:first'),
+				$labelItem = $element.find('.label_item:first');
+
+			//파괴일 때
+			if(method === 'destroy') {
+				$element.removeClass('focus active');
+				$element.add($labelItem).off('.label');
+
+			//초기화일 때
+			}else if($element.is(':reset')) {
+				$element.on('click.label', function(event) {
+					var $form = $element.parents('form');
+
+					//요소가 있을 때
+					if($form.length) {
+						//초기화
+						$form[0].reset();
+
+						//새로고침
+						$form.find('.label').label('refresh');
+
+						event.preventDefault();
+					}
+				});
+
+			//새로고침
+			}else if(method === 'refresh') {
+				$labelItem.triggerHandler('change.label');
+
+			//포커스
+			}else if(method === 'focus') {
+				$labelItem.focus();
+
+			//초기화
+			}else if(method === 'reset') {
+				$labelItem.val(($labelItem.is('select[multiple]')) ? NaN : '');
+				$element.label('refresh');
+
+			//값
+			}else if(method === 'val') {
+				//값이 있을 때
+				if(hasValue) {
+					//문자일 때
+					if(valueIsString) {
+						$labelItem.val(value);
+						$element.label('refresh');
+					}
+				}else{
+					result.push({
+						$element : $element,
+						value : $labelItem.val() || ''
+					});
+				}
+
+			//기본
+			}else if(method === 'default') {
+				//값이 있을 때
+				if(hasValue) {
+					//문자일 때
+					if(valueIsString) {
+						_defaultLabelText = value;
+
+						$labelItem.triggerHandler('label:setDefault.label');
+
+						$element.label('refresh');
+
+						//초기화
+						_defaultLabelText = undefined;
+					}
+				}else{
+					$labelItem.triggerHandler('label:getDefault.label');
+
+					result.push({
+						$element : $element,
+						value : _defaultLabelText
+					});
+
+					//초기화
+					_defaultLabelText = undefined;
+				}
+			}else{
+				var defaultLabelText = $labelText.text();
+
+				//파괴
+				$element.label('destroy');
+
+				$labelItem.on('change.label', function(event) {
+					var tagName = this.tagName,
+						type = this.type,
+						labelText = [];
+
+					//셀렉트일 때
+					if(tagName === 'SELECT') {
+						var $selectedOption = $labelItem.find('option:selected'),
+							selectedOptionLength = $selectedOption.length;
+
+						for(var i = 0; i < selectedOptionLength; i++) {
+							labelText[i] = $selectedOption.eq(i).text();
+						}
+
+						labelText = labelText.join(', ');
+
+						//값이 없을 때
+						if(!labelText) {
+							labelText = defaultLabelText;
+						}
+
+						//선택된 옵션이 있을 때
+						if(selectedOptionLength) {
+							$element.addClass('active');
+						}else{
+							$element.removeClass('active');
+						}
+
+						$labelText.text(labelText);
+
+					//인풋일 때
+					}else if(tagName === 'INPUT') {
+						//체크박스일 때
+						if(type === 'checkbox') {
+							//체크되어 있을 때
+							if(this.checked) {
+								$element.addClass('active');
+							}else{
+								$element.removeClass('active');
+							}
+
+						//라디오일 때
+						}else if(type === 'radio') {
+							var $labelRadioItem = $('.label.radio .label_item');
+
+							for(var i = 0, labelRadioItemLength = $labelRadioItem.length; i < labelRadioItemLength; i++) {
+								var $labelRadio = $labelRadioItem.eq(i).parents('.label.radio');
+
+								//체크되어 있을 때
+								if($labelRadioItem[i].checked) {
+									$labelRadio.addClass('active');
+								}else{
+									$labelRadio.removeClass('active');
+								}
+							}
+
+						//파일일 때
+						}else if(type === 'file') {
+							var files = this.files || this.value;
+
+							//파일이 없을 때
+							if(!files) {
+								files = [];
+							}
+
+							//문자일 때
+							if(typeof files === 'string') {
+								files = [files];
+							}
+
+							var filesLength = files.length;
+
+							for(var i = 0; i < filesLength; i++) {
+								var file = files[i];
+
+								labelText[i] = file.name || file;
+							}
+
+							labelText = labelText.join(', ');
+
+							//값이 없을 때
+							if(!labelText) {
+								labelText = defaultLabelText;
+							}
+
+							$labelText.text(labelText);
+
+							//파일이 선택되어 있을 때
+							if(filesLength) {
+								$element.addClass('active');
+							}else{
+								$element.removeClass('active');
+							}
+						}
+					}
+				}).on('focusin.label', function(event) {
+					$element.addClass('focus');
+				}).on('focusout.label', function(event) {
+					$element.removeClass('focus');
+				}).on('label:getDefault.label', function(event) {
+					_defaultLabelText = defaultLabelText;
+				}).on('label:setDefault.label', function(event) {
+					defaultLabelText = _defaultLabelText;
+				});
+
+				//새로고침
+				$element.label('refresh');
+			}
+		});
+
+		var resultLength = result.length;
+
+		//결과가 있을 때
+		if(resultLength === 1) {
+			result = result[0].value;
+
+		//결과가 없을 때
+		}else if(!resultLength) {
+			result = this;
+		}
+
+		return result;
+	};
+})(window.jQuery);
